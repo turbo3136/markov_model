d618ab26b85941a693d5ce061249a5ab457f3cb3
turbo <brent@videoblocks.com>
2019-03-30 18:22:04 -0400

    markov_model/MarkovState.py - update MarkovState to include time_step argument

--- markov_model/MarkovState.py
+++ markov_model/MarkovState.py
@@ -2,18 +2,25 @@ class MarkovState:
     """Create a MarkovState object with identifying attributes
 
     Keyword arguments:
-        state_id -- unique identifier
+        state_id -- unique identifier for properties of state other than the time_step
+        time_step -- time step of the current state
         state_name -- optional, display name. Set to state_id if None
         state_description -- optional, readable description of the state
     """
 
-    def __init__(self, state_id, state_name=None, state_description=None):
+    def __init__(self, state_id, time_step, state_name=None, state_description=None):
         if not state_name:  # if we weren't provided a state_name, set it to state_id
             state_name = state_id
 
         self.state_id = state_id
+        self.time_step = time_step
         self.state_name = state_name
         self.state_description = state_description
 
     def __repr__(self):
-        return 'MarkovState(state_id={}, state_name={})'.format(self.state_id, self.state_name)
+        return 'MarkovState(state_id={}, time_step={})'.format(self.state_id, self.time_step)
+
+
+if __name__ == '__main__':
+    s1 = MarkovState(state_id='test_id1', time_step=0)
+    print(s1.time_step)
