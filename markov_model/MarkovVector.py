f16dd94d087b3a315a750ac59e808ce2f9b5d47e
turbo <brent@videoblocks.com>
2019-03-30 22:52:36 -0400

    markov_model/MarkovVector.py - create MarkovVector class

---
+++ markov_model/MarkovVector.py
@@ -0,0 +1,14 @@
+class MarkovVector:
+    """Create a MarkovVector object defining the available state space
+
+    Keyword arguments:
+        state_list -- list of MarkovState objects defining the state space
+        time_step -- time step of the current vector
+    """
+
+    def __init__(self, state_list, time_step):
+        self.state_list = state_list
+        self.time_step = time_step
+
+    def __repr__(self):
+        return 'MarkovState(state_id={}, time_step={})'.format(self.state_list, self.time_step)
