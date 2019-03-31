761e9f85e487c68bb7af0da50aee0a763f5ae016
turbo <brent@videoblocks.com>
2019-03-30 22:47:34 -0400

    markvo_model/TransitionFunction - fix indent on docstring

--- markov_model/TransitionFunction.py
+++ markov_model/TransitionFunction.py
@@ -1,11 +1,11 @@
 class TransitionFunction:
     """Create a TransitionFunction object defining the probability of transitioning between two MarkovStates
 
-        Keyword arguments:
-            state_id_tuple -- unique identifier, tuple containing the id of the initial state and the end state
-            transition_function -- time dependent function representing the transition probability between
-                initial and end states. transition_function must take exactly one argument, t (time_step)
-        """
+    Keyword arguments:
+        state_id_tuple -- unique identifier, tuple containing the id of the initial state and the end state
+        transition_function -- time dependent function representing the transition probability between
+            initial and end states. transition_function must take exactly one argument, t (time_step)
+    """
 
     def __init__(self, state_id_tuple, transition_function):
         self.state_id_tuple = state_id_tuple
