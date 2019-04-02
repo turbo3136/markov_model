8931f2938043b545a51f05716b4f42e903862fee
turbo <brent@videoblocks.com>
2019-04-01 23:41:53 -0400

    markov_model/MarkovChain.py - add history to MarkovChain object and clean up the code

--- markov_model/MarkovChain.py
+++ markov_model/MarkovChain.py
@@ -13,26 +13,30 @@ class MarkovChain:
     """
 
     def __init__(self, initial_state, state_space, transition_matrix, total_steps):
-        self.initial_state = initial_state  # reminder: this is a MarkovStateVector object
-        self.state_space = state_space  # reminder: this is a MarkovStateSpace object
-        self.transition_matrix = transition_matrix  # reminder: this is a MarkovTransitionMatrix object
-        self.total_steps = total_steps
-
         # do a few value checks on the sizes of the states and matrix
-        if self.state_space.size != len(self.initial_state.state_distribution):
+        if state_space.size != len(initial_state.state_distribution):
             raise ValueError(
                 'MarkovChain.state_space must be the same size as MarkovChain.initial_state.state_distribution'
             )
-        if self.state_space.size != self.transition_matrix.transition_function_matrix.shape[0]:
+        if state_space.size != transition_matrix.transition_function_matrix.shape[0]:
             raise ValueError(
                 '''MarkovChain.transition_matrix must be a square matrix 
                 with each dimension the size of MarkovChain.state_space'''
             )
 
+        self.initial_state = initial_state  # reminder: this is a MarkovStateVector object
+        self.state_space = state_space  # reminder: this is a MarkovStateSpace object
+        self.transition_matrix = transition_matrix  # reminder: this is a MarkovTransitionMatrix object
+        self.total_steps = total_steps
+        self.history = np.array([self.initial_state])  # initialize the history with the initial state
+
+        # then we calculate the current state and log the history along the way
+        self.current_state = self.state_after_n_steps(self.initial_state, self.total_steps, log_history=True)
+
     def __repr__(self):
         return 'MarkovChain(initial_state={}, state_space={})'.format(self.initial_state, self.state_space)
 
-    def next_state(self, starting_state):
+    def next_state(self, starting_state, log_history=False):
         """return a MarkovStateVector object after applying the transition matrix"""
         next_state = copy.deepcopy(starting_state)  # make a copy of the starting MarkovStateVector so we can update it
         next_state.time_step += 1  # add 1 to the starting time_step
@@ -42,21 +46,25 @@ class MarkovChain:
             self.transition_matrix.matrix_at_time_step(starting_state.time_step)
         )
 
+        # check to see if we want to log the history
+        if log_history:
+            self.history = np.append(self.history, next_state)  # add the next state to the history log
+
         return next_state
 
-    def non_vectorized_state_after_n_steps(self, starting_state, n):
+    def state_after_n_steps(self, starting_state, n, log_history=False):
         """return a MarkovStateVector object after applying n transitions"""
         current_state = copy.deepcopy(starting_state)
 
         for step in np.arange(n):
-            current_state = self.next_state(current_state)
+            current_state = self.next_state(current_state, log_history)
 
         return current_state
 
-    def state_after_n_steps(self, starting_state, n):
+    def vectorized_state_after_n_steps(self, starting_state, n, log_history=False):
         """return a MarkovStateVector object after applying n transitions
 
         vectorize to allow for array inputs for either starting_state or n, not both
         """
-        vectorized_func = np.vectorize(self.non_vectorized_state_after_n_steps)
-        return vectorized_func(starting_state, n)
+        vectorized_func = np.vectorize(self.state_after_n_steps)
+        return vectorized_func(starting_state, n, log_history)
